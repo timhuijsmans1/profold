16769e94a64640eed277557484378fc980da4909
Bas Huijsmans <MacbookBas@mbp-van-bas-2.home>
2020-06-05 10:49:25 +0200

    Added connection class

--- testrandom.py
+++ testrandom.py
@@ -8,11 +8,15 @@ class Matrix:
         can be populated by amino acids"""
     
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        
         # initiate matrix with 0's
         self.matrix = [[0]*width for n in range(height)]
+    
+    def update_matrix(self, row, column, value):
+        self.matrix[row][column] = value
+        return self.matrix
+    
+    def get_matrix(self):
+        return self.matrix
 
     
 class AminoAcid:
@@ -24,27 +28,31 @@ class AminoAcid:
         self.column = int(column)
         self.value = value
     
-    # update methods for next amino acid
-    def step_right(self):
-        self.row += 1
-        return self.row
+    def update_position(self,row_step,column_step):
+        self.row += row_step
+        self.column += column_step
         
-    def step_left(self):
-        self.row += -1
-        return self.row
+        return self.row, self.column
     
-    def step_up(self):
-        self.column += 1
-        return self.column
-        
-    def step_down(self):
-        self.column += -1
-        return self.column
-        
     def update_value(self, value):
         self.value = value
         return self.value
 
+class Connection:
+    """Store the connections of the previous amino acid"""
+    def __init__(self):
+        self.connections = {}
+        
+    def set_connections(self, direction, row, column):
+        self.connections[direction] = [row,column]
+        return self.connections
+        
+    def get_random_connection(self):
+        return random.choice(list(self.connections.items()))
+        
+    def clear_connections(self):
+        self.connections = {}
+        return self.connections
 
 class Protein:
     """Keeps track of all the amino acids placed in the grid
@@ -57,12 +65,15 @@ class Protein:
     def add_amino_acid(self, row, column, value):
         self.protein.append([row, column, value])
         return self.protein
+        
+    def get_protein(self):
+        return self.protein
 
 def matrix_initializer(width,height):
     """initialize the matrix with first amino acid at the center"""
     
     # create matrix object
-    initial_matrix = Matrix(width,height).matrix
+    initial_matrix = Matrix(width,height)
     
     # define the string to map as a list of H or P values:
     #protein_string = ["H","H","P","H","H","H","P","H","P","H","H","H","P","H"]
@@ -72,7 +83,7 @@ def matrix_initializer(width,height):
     amino_acid = AminoAcid(height/2, width/2, protein_string[0])
     
     # place this first amino acid in the middle of the matrix
-    initial_matrix[amino_acid.row][amino_acid.column] = amino_acid.value
+    initial_matrix.update_matrix(amino_acid.row, amino_acid.column, amino_acid.value)
     
     # add coordinates and value to protein
     protein = Protein()
@@ -87,47 +98,39 @@ def matrix_updater(matrix, protein_string, amino_acid, protein):
     """Update the matrix with the remaining values of the protein string"""
     
     # place all other amino acids in the string on a random connected point
+    print(protein_string)
     for value in protein_string:    
         
-        # check available positions around the most recent placed amino_acid and add to list
-        available_positions = []
-        if matrix[amino_acid.row + 1][amino_acid.column] == 0:
-            available_positions.append("+1")
+        connections = Connection()
+        # check available positions in the x dimension
+        if matrix.get_matrix()[amino_acid.row + 1][amino_acid.column] == 0:
+            connections.set_connections("right", 1, 0)
         
-        if matrix[amino_acid.row - 1][amino_acid.column] == 0:
-            available_positions.append("-1")
+        if matrix.get_matrix()[amino_acid.row - 1][amino_acid.column] == 0:
+            connections.set_connections("left", -1, 0)
+        
+        # check available posisitions in the y dimension    
+        if matrix.get_matrix()[amino_acid.row][amino_acid.column + 1] == 0:
+            connections.set_connections("up", 0, 1)
             
-        if matrix[amino_acid.row][amino_acid.column + 1] == 0:
-            available_positions.append("+2")
+        if matrix.get_matrix()[amino_acid.row][amino_acid.column - 1] == 0:
+            connections.set_connections("down", 0, -1)
             
-        if matrix[amino_acid.row][amino_acid.column - 1] == 0:
-            available_positions.append("-2")
+        # pick a random connection from the connections
+        step = connections.get_random_connection()
         
-        # if only one position available, choose that one
-        if len(available_positions) == 1:
-            random_direction = available_positions[0]
-        # randomly choose one of the directions from the list of possible directions 
-        elif len(available_positions) > 1: 
-            random_direction = available_positions[random.randint(0,len(available_positions)-1)]
-        
-        # update the new position of the amino acid
-        if random_direction == "+1":
-            amino_acid.step_right()
-        if random_direction == "-1":
-            amino_acid.step_left()
-        if random_direction == "+2":
-            amino_acid.step_up()
-        if random_direction == "-2":
-            amino_acid.step_down()
-        
-        # update the value of the amino acid with P or H
         amino_acid.update_value(value)
         
         # update matrix with next amino acid
-        matrix[amino_acid.row][amino_acid.column] = amino_acid.value
+        matrix.update_matrix(amino_acid.row + step[1][0], amino_acid.column + step[1][1], amino_acid.value)
         
         # add the the coordinates and value of the amino acid to the Protein
-        protein.add_amino_acid(amino_acid.row, amino_acid.column, amino_acid.value)
+        protein.add_amino_acid(amino_acid.row + step[1][0], amino_acid.column + step[1][1], amino_acid.value)
+        
+        # update the value and position of the amino acid
+        amino_acid.update_position(step[1][0], step[1][1])
+        
+        connections.clear_connections()
         
     return matrix, protein
         
@@ -144,10 +147,10 @@ def main():
     
     
     """Output: h*w matrix showing the folded protein and the list of aminoacid location in order of bonds"""    
-    for row in matrix[0]:
+    for row in matrix[0].get_matrix():
         print (row)
     print(matrix[1].protein)
-    plt.imshow(matrix[0], cmap=plt.cm.bwr)
+    plt.imshow(matrix[0].get_matrix(), cmap=plt.cm.bwr)
     plt.show()
     
 if __name__ == "__main__":
